aeb252ddb43ce8e1682aa75886d2ddd292d49444
xchong0731 <xchong1@stevens.edu>
2016-04-07 14:19:01 -0400

    Added files via upload

--- HomePage.java
+++ HomePage.java
@@ -13,7 +13,17 @@ public HomePage(){
 	label1.setFont(new Font("times", Font.BOLD,28));
 	p1.add(label1);
 	c.add(BorderLayout.NORTH,p1);
-	
+	JPanel p2=new JPanel();
+	p2.setLayout(new FlowLayout(FlowLayout.CENTER,20,20));
+	JComboBox s1=new JComboBox();
+	JComboBox s2=new JComboBox();
+	JComboBox s3=new JComboBox();
+	JButton b1=new JButton("OK");
+	p2.add(s1);
+	p2.add(s2);
+	p2.add(s3);
+	p2.add(b1);
+	c.add(BorderLayout.CENTER,p2);
 	
 	setSize(1000,600);
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
